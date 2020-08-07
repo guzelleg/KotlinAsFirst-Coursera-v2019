5b80e562701c2eea3b38611c680a75c8c7118b0e
Guzel <guzel.gimadieva@erstegroup.com>
2020-08-07 20:22:59 +0200

    gotovo2

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -1,9 +1,6 @@
 @file:Suppress("UNUSED_PARAMETER")
 
 package lesson1.task1
-
-import org.junit.Assert.assertEquals
-import org.junit.Test
 import kotlin.math.*
 
 /**
@@ -61,7 +58,7 @@ fun main() {
  * Пользователь задает время в часах, минутах и секундах, например, 8:20:35.
  * Рассчитать время в секундах, прошедшее с начала суток (30035 в данном случае).
  */
-fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
+fun seconds(hours: Int, minutes: Int, seconds: Int): Int = hours*60*60 + minutes*60 + seconds
 
 /**
  * Тривиальная
@@ -70,7 +67,7 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = TODO()
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = TODO()
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = (48 *sagenes + 16 * arshins + vershoks)*0.04445
 
 /**
  * Тривиальная
@@ -86,7 +83,7 @@ fun angleInRadian(deg: Int, min: Int, sec: Int): Double = Math.PI/180.0 * (deg +
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = TODO()
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1))
 
 /**
  * Простая
@@ -103,7 +100,7 @@ fun thirdDigit(number: Int): Int = number/100%10
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = TODO()
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int =(hoursArrive*60 + minutesArrive) - (hoursDepart*60 + minutesDepart)
 
 /**
  * Простая
@@ -112,7 +109,7 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
+fun accountInThreeYears(initial: Int, percent: Int): Double = initial * (1 + percent / 100.0).pow(3)
 
 /**
  * Простая
@@ -120,9 +117,4 @@ fun accountInThreeYears(initial: Int, percent: Int): Double = TODO()
  * Пользователь задает целое трехзначное число (например, 478).
  * Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
-fun numberRevert(number: Int): Int = TODO()
-
-@Test
-private fun check(){
-    assertEquals(4, sqr(2))
-}
+fun numberRevert(number: Int): Int = (number%10*100) + (number/10%10*10) + (number/100)
